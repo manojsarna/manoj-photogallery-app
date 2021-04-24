de9e256055950fa83ee8bdc8665fa4dc4dde41a2
Manoj Sarna <79917843+manojsarna@users.noreply.github.com>
2021-04-24 15:24:49 +0530

    update

---
+++ src/comps/progressBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import useStorage from '../hooks/useStorage';
+
+constProgressBar = () => {
+
+    return(
+
+        <div className="progress-bar">progress</div>
+    )
+
+}
+
+export default ProgressBar;
