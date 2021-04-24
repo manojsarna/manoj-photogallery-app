9f305428f00480175cddca0b2fa541f257e32480
Manoj Sarna <79917843+manojsarna@users.noreply.github.com>
2021-04-24 15:20:36 +0530

    update

---
+++ src/comps/UploadForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import ProgressBar from './progressBar';
+
+const UploadForm = () => {
+    
+    const [file, setFile] = useState(null);
+    const [error,setError] = useState(null);
+
+    const types = ['image/png','image/jpeg']
+
+    const changeHandler = (e) => {
+
+        //console.log("Changed")
+        let selected = e.target.files[0]
+        //console.log(selected)
+
+        if(selected && types.includes(selected.type)){
+
+            setFile(selected)
+            setError('')
+        } else {
+
+            setFile(null)
+            setError('Please select image file ( png or jpeg )')
+
+        }
+    }
+
+    return (
+        <form>
+            <label>
+                <input type="file" onChange= {changeHandler} />
+                <span>+</span>
+            </label>
+            <div className="output">
+                {error && <div className="error"> { error } </div> }
+
+                {file && <div>{ file.name } </div> }
+
+                {file  && <ProgressBar file={file} setFile={setFile} />}
+            </div>
+            
+
+        </form>
+
+    )
+}
+
+export default UploadForm;
